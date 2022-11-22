5a7d36ba4ff7025a6361ce5329ac22fa7c819fb4
salbrec <salbrec@uni-mainz.de>
2022-11-22 12:52:44 +0100

    add genmod and antibody

---
+++ genetic_mod_example.py
@@ -0,0 +1,31 @@
+import os
+from json_parser.main_parser import EncodeParser
+import pandas as pd
+import random
+import json
+
+# the parser automatically downloads large JSON files for the experiments
+# here you can specify where those files are stored
+# this folder is checked automatically, so JSONs are downloaded again and again
+parser = EncodeParser('../../encode_jsons/genmod_test/',force_json_update=True)
+
+accessions = ['ENCSR000EHA', 'ENCSR140GLO', 'ENCSR668LDD']
+for accession in accessions:
+    print('Parsing the dataset', accession)
+    genmod_info = parser.get_genetic_mods_for_dataset(accession)
+
+    print(genmod_info)
+
+    print('\n%s\n'%(80*'#'))
+
+
+
+
+
+
+
+
+
+
+
+
--- json_parser/main_parser.py
+++ json_parser/main_parser.py
@@ -14,8 +14,8 @@ class EncodeParser:
     def get_biosample_for_dataset(self, exp_accession):
         headers = {'accept': 'application/json'}
         biosample_info = {'avail':False, 'n_replicates':0, 'biosample_jsons':[],
-            'biosample_ids':[], 'biosample_uuids':[], 'uniq_biosample':None,
-            'custom_ids':[], 'donor_ids':[]}
+            'biosample_IDs':[], 'biosample_uuids':[], 'uniq_biosample':None,
+            'custom_IDs':[], 'donor_IDs':[]}
 
         exp = None
         json_fp = '%s%s.json'%(self.json_dir, exp_accession)
@@ -43,7 +43,7 @@ class EncodeParser:
                 return biosample_info
 
             biosample_info['avail'] = True
-            biosample_info['biosample_ids'].append(biosample['@id'])
+            biosample_info['biosample_IDs'].append(biosample['@id'])
             biosample_info['biosample_uuids'].append(biosample['uuid'])
             biosample_info['biosample_jsons'].append(biosample)
             custom_id = ''
@@ -51,13 +51,83 @@ class EncodeParser:
             donor_id = 'NA'
             if 'donor' in biosample:
                 donor_id = biosample['donor']['@id']
-            biosample_info['donor_ids'].append(donor_id)
+            biosample_info['donor_IDs'].append(donor_id)
 
-        if len(set(biosample_info['biosample_ids'])) == 1:
+        if len(set(biosample_info['biosample_IDs'])) == 1:
             biosample_info['uniq_biosample'] = biosample_info['biosample_jsons'][0]
-        if len(set(biosample_info['donor_ids'])) == 1:
+        if len(set(biosample_info['donor_IDs'])) == 1:
             biosample_info['uniq_biosample'] = biosample_info['biosample_jsons'][0]
 
         return biosample_info
 
+    def get_genetic_mods_for_dataset(self, exp_accession):
+        headers = {'accept': 'application/json'}
+        genmod_info = {'modified':False, 'genmod_IDs':[], 'short_descr':[],
+            'n_replicates':0}
+
+        exp = None
+        json_fp = '%s%s.json'%(self.json_dir, exp_accession)
+        if self.json_dir != None and os.path.exists(json_fp) and not self.force_json_update:
+            exp = json.load(open(json_fp,'r'))
+        else:
+            url = 'https://www.encodeproject.org/experiments/%s/?frame=object&frame=embedded'%(exp_accession)
+            exp = requests.get(url, headers={'accept': 'application/json'}).json()
+            if self.json_dir != None:
+                open(json_fp,'w').write(json.dumps(exp, indent=4))
+
+        replicates = exp.get('replicates',None)
+        if replicates == None:
+            print('Dataset %s has no replicates.'%(exp_accession))
+            return genmod_info
+        genmod_info['n_replicates'] = len(replicates)
+        for repl_i, repl in enumerate(replicates):
+            library = repl.get('library',None)
+            if library == None:
+                print('Replicate %d of dataset %s has no library.'%(repl_i, exp_accession))
+                return genmod_info
+            biosample = library.get('biosample',None)
+            if biosample == None:
+                print('Replicate %d of dataset %s has no biosample in library.'%(repl_i, exp_accession))
+                return genmod_info
+
+            genmods = biosample.get('genetic_modifications',[])
+            genmod_info['genmod_IDs'] += genmods
+            if len(genmods) > 0:
+                genmod_info['modified'] = True
+                if 'applied_modifications' in biosample:
+                    for modi in biosample['applied_modifications']:
+                        short = '%d_%s_%s_%s'%(repl_i, modi.get('category','NA'),
+                            modi.get('purpose','NA'), modi.get('method','NA'))
+                        genmod_info['short_descr'].append(short)
+
+        return genmod_info
+
+    def get_antibody_for_ChIP(self, exp_accession):
+        headers = {'accept': 'application/json'}
+        antibody_info = {'n_replicates':0, 'antibody_IDs':[], 'antibodies':[]}
+
+        exp = None
+        json_fp = '%s%s.json'%(self.json_dir, exp_accession)
+        if self.json_dir != None and os.path.exists(json_fp) and not self.force_json_update:
+            exp = json.load(open(json_fp,'r'))
+        else:
+            url = 'https://www.encodeproject.org/experiments/%s/?frame=object&frame=embedded'%(exp_accession)
+            exp = requests.get(url, headers={'accept': 'application/json'}).json()
+            if self.json_dir != None:
+                open(json_fp,'w').write(json.dumps(exp, indent=4))
+
+        replicates = exp.get('replicates',None)
+        if replicates == None:
+            print('Dataset %s has no replicates.'%(exp_accession))
+            return antibody_info
+        antibody_info['n_replicates'] = len(replicates)
+        for repl_i, repl in enumerate(replicates):
+            antibody = repl.get('antibody',None)
+            if antibody == None:
+                print('Replicate %d of dataset %s has no antibody.'%(repl_i, exp_accession))
+                return antibody_info
+            antibody_info['antibody_IDs'].append(antibody['@id'])
+            antibody_info['antibodies'].append(antibody)
+
+        return antibody_info
 
